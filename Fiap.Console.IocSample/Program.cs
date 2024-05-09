82c85213fdb031076db93fdd35ea2963b7179f1f
Flavio Moreni <PF0954@fiap.com.br>
2024-05-09 00:09:56 -0300

    versao tradicional

--- Fiap.Console.IocSample/Program.cs
+++ Fiap.Console.IocSample/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Mensageiro mensageiro = new Mensageiro();
+        mensageiro.EnviarMensagem("Olá, mundo!");
+    }
+}
